e1082c8815cc87428a2965f881043e55cc6d95da
Akhi.Gupta <akhil.gupta@albertsons.com>
2021-05-30 19:51:08 +0530

    Adding Method to verify the Column Name Values

--- tests/grid.test.ts
+++ tests/grid.test.ts
@@ -2,21 +2,40 @@ import { expect } from "chai";
 import { GridPage } from "../pages/gridPage";
 import { Page } from "../pages/page";
 
-describe("Load AG Grid", () => {
+describe("Should Validate the AG Grid Data", () => {
+  
+  /** 
+   * Open the Application 
+  */
   before(() => {
     Page.open();
   });
 
-  it("Should display the grids", () => {
+  it("Should display the grids Header Value", () => {
     GridPage.mainGrid.waitForDisplayed();
 
     /**
-     * I'm using forEach here for readability purposes
-     * The function iterates through the element array
-     * and checks if element is displayed.
+     * The function iterates through the Column Header array and check it display Correct or not
      */
     GridPage.columnHeader.forEach((el) => {
       expect(el.isDisplayed()).to.eql(true);
     });
+
+   
+}),
+
+it("Should display the grids Column Name Header", () => {
+  GridPage.mainGrid.waitForDisplayed();
+
+  /**
+   * The function iterates through the Column Header Name and display correct or not
+   */
+
+  GridPage.colName.forEach((el) => {
+    expect(el.isDisplayed()).to.eql(true);
   });
+
+
+});
+
 });
